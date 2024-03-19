ba1345e39b927d2fa6cd6691c81411857e5f8e26
djutras2 <dustin.jutras@gmail.com>
2024-03-18 23:40:19 -0400

    fan

---
+++ Scripts/Session/fan.gd
@@ -0,0 +1,17 @@
+extends Area3D
+class_name Fan
+
+@export var direction : Vector3
+@export var strength : float
+
+#func _ready():
+	#body_entered.connect(_body_entered)
+	#body_exited.connect(_body_entered)
+#
+#func _body_entered(body):
+	#if(body as Sled):
+		#var sled = body as Sled
+		
+func _physics_process(delta):
+	for body in get_overlapping_bodies():#.filter(func(b): b as Sled):
+		(body as Sled).velocity += direction.normalized() * strength * Vector3.UP * delta
